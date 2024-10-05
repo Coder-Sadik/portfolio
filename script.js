dac2da65a8265d4d63ec583cdcc4caf5db01bdef
Sadik Rahman <srahman2702@gmail.com>
2024-10-05 22:40:46 +0600

    skills section added

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const toggler = (e) => {
 	e.classList.toggle("toggle");
 	navMenu.classList.toggle("active");
 };
+
+//skill progress bar automation
+document
+	.querySelectorAll(".skill-details")
+	.forEach(function (skillDetail, index) {
+		const percentageText = skillDetail.querySelector(".percentage").textContent;
+		const percentageValue = parseInt(percentageText); // Extract the number
+
+		// Select the corresponding progress bar
+		const progressBar = document.querySelectorAll(".skill-percentage")[index];
+		progressBar.style.width = percentageValue + "%";
+	});
